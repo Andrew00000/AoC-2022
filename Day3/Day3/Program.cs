8f0efc461151c1caf5b5062789c9f812d4d5173d
aft <aft@captureone.com>
2022-12-03 15:15:18 +0100

    .

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+var input = File.ReadAllLines($@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\in.txt");
+
+var sum = 0;
+var sum2 = 0;
+var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+foreach (var line in input)
+{
+    var set1 = line[..(line.Length/2)].ToHashSet();
+    var set2 = line[(line.Length / 2)..].ToHashSet();
+
+    var set3 = set1.Intersect(set2);
+
+
+    sum += alphabet.IndexOf(set3.First()) + 1;
+}
+
+
+for (int i = 0; i < input.Length; i+=3)
+{
+    var set1 = input[i].ToHashSet();
+    var set2 = input[i+1].ToHashSet();
+    var set3 = input[i+2].ToHashSet();
+
+    var intersect = set1.Intersect(set2).Intersect(set3);
+
+    sum2 += alphabet.IndexOf(intersect.First()) + 1;
+}
+
+Console.WriteLine(sum); 
+Console.WriteLine(sum2);
