22ff48c6de9ffd21fdd9ee5c96828cab5596d7a0
aft <aft@captureone.com>
2022-12-07 15:20:15 +0100

    .

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -4,6 +4,12 @@ var rootFolder = Parser.Parse(input);
 
 var allFolders = rootFolder.GetAllChildrenFoldersAndSubFolders().ToList();
 
+var sum = 0L;
+
+allFolders.Where(f => f.GetSize() <= 100000).Select(f => f.GetSize()).ToList().ForEach(s => sum += s);
+
+Console.WriteLine(sum);
+
 var totalDiskSpace = 70000000;
 
 var requiredDiskSpace = 30000000;
@@ -13,12 +19,6 @@ var usedDiskSpace = rootFolder.GetSize();
 var freeDiskSpace = totalDiskSpace - usedDiskSpace;
 
 var spaceNeededToBeFreedUp = requiredDiskSpace - freeDiskSpace;
-
-var sum = 0L;
-
-allFolders.Where(f => f.GetSize() <= 100000).Select(f => f.GetSize()).ToList().ForEach(s => sum += s);
-
-Console.WriteLine(sum);
 
 if (spaceNeededToBeFreedUp < 0)
 {
