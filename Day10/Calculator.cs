ef43ffbc34cdc47b5f035379af3326f8cb7a5d44
aft <aft@captureone.com>
2022-12-10 19:06:00 +0100

    .

--- Day10/Calculator.cs
+++ Day10/Calculator.cs
@@ -27,14 +27,13 @@
     internal void Print(int[] ints)
     {
         x = 1L;
-
-        var targets = new long[] { 39, 79, 119, 159, 199, 239 };
+        var width = 40;
 
         for (int i = 0; i < ints.Length; i++)
         {
             var pixel = ' ';
 
-            var ShouldPrint = x - 1 <= i % 40 && i % 40 <= x + 1;
+            var ShouldPrint = x - 1 <= i % width && i % width <= x + 1;
 
             if (ShouldPrint)
             {
@@ -43,7 +42,7 @@
 
             Console.Write(pixel);
 
-            if (targets.Contains(i))
+            if ((i + 1) % width == 0)
             {
                 Console.WriteLine();
             }
