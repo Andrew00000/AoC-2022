366ac8c201ec69ddee2be2655bc9f063cd9c0b72
aft <aft@captureone.com>
2022-12-14 14:19:28 +0100

    Revert "."
    This reverts commit 106dc3f3711e594b25325a32e1bb259d0f553565.

--- Day13/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿var input = File.ReadAllText($@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\min.txt");
-
-var parser = new Parser();
-
-var bla = parser.Parse(input);
