6d10fcade1fad54f6c0f3412bb16e8128b3f5804
Andrew00000 <62332857+Andrew00000@users.noreply.github.com>
2023-05-16 19:06:40 +0200

    Day 3 Cleanup

--- Day3/Day3/Program.cs
+++ Day3/Day3/Program.cs
@@ -1,23 +1,19 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿var input = File.ReadAllLines($@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\in.txt");
+const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var input = File.ReadAllLines($@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\in.txt");
 
 var sum = 0;
-var sum2 = 0;
-var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 foreach (var line in input)
 {
     var set1 = line[..(line.Length/2)].ToHashSet();
     var set2 = line[(line.Length / 2)..].ToHashSet();
-
     var set3 = set1.Intersect(set2);
-
 
     sum += alphabet.IndexOf(set3.First()) + 1;
 }
+Console.WriteLine(sum); 
 
-
+var sum2 = 0;
 for (int i = 0; i < input.Length; i+=3)
 {
     var set1 = input[i].ToHashSet();
@@ -28,6 +24,4 @@ for (int i = 0; i < input.Length; i+=3)
 
     sum2 += alphabet.IndexOf(intersect.First()) + 1;
 }
-
-Console.WriteLine(sum); 
 Console.WriteLine(sum2);
