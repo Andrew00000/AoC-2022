093ea2585c09986a0cb04e5dd85b28668e4a7e2f
aft <aft@captureone.com>
2023-09-15 12:25:21 +0200

    Day 4 done

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,19 +1,30 @@
-﻿using System.Linq.Expressions;
+﻿var input = File.ReadAllLines($@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\in.txt");
+var solutionOne = 475;
+var solutionTwo = 825;
 
-var input = File.ReadAllLines($@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\in.txt");
+
+//Problem One: https://adventofcode.com/2022/day/4
 
 var parser = new Parser();
 var pairAssigments = new List<(Assignment, Assignment)>();
 
-foreach(var line in input)
+foreach (var line in input)
 {
-    pairAssigments.Add(parser.Parse(line)) ;
+    pairAssigments.Add(parser.Parse(line));
 }
 
 var numberOfFullContains = pairAssigments.Where(v => v.Item1.IsContatining(v.Item2) || v.Item2.IsContatining(v.Item1)).Count();
 
-Console.WriteLine(numberOfFullContains);
+var problemOneResult = solutionOne == numberOfFullContains ? $"Yes the answer is {numberOfFullContains}"
+                                                           : $"No the answer isnt {numberOfFullContains}";
+
+Console.WriteLine(problemOneResult);
+
+//Problem Two: https://adventofcode.com/2022/day/4
 
 var numberOfOverlappingPairs = pairAssigments.Where(v => v.Item1.IsOverLapping(v.Item2)).Count();
 
-Console.WriteLine(numberOfOverlappingPairs);
+var problemTwoResult = solutionTwo == numberOfOverlappingPairs ? $"Yes the answer is {numberOfOverlappingPairs}"
+                                                               : $"No the answer isnt {numberOfOverlappingPairs}";
+
+Console.WriteLine(problemTwoResult);
